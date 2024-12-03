9f07fc207ee9d84983716b0456cf6ade39bd7cb0
engrsakib <snazmussakib01@gmail.com>
2024-12-04 01:17:22 +0600

    footer section added

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,22 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <header className='container mx-auto'>
+      <header className="container mx-auto">
         <Header></Header>
       </header>
-      <main className='container mx-auto'>
-        
-      </main>
+      <main className="container mx-auto min-h-screen"></main>
       <footer>
-        
+        <Footer></Footer>
       </footer>
     </>
-  )
+  );
 }
 
 export default App
